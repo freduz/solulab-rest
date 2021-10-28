20af5f67a4ab2ac81e846257d17c83a7c316d115
Fredy Daniel <fredyd900@gmail.com>
2021-10-28 08:54:09 +0530

    Secrity features added

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ const productRoute = require('./routes/productRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/errorHandler');
+const ratelimiter = require('express-rate-limit');
+const helemt = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 app.use(express.json());
 dotenv.config({});
+app.use(mongoSanitize());
+app.use(helemt());
 require('./config/dbconfig')();
+
+const limiter = ratelimiter({
+  max: 4,
+  windowMs: 60 * 60 * 1000,
+  message: 'So many request attempt from this IP addresss',
+});
+
+//@ Login attempt prevented using express limiter
+app.use('/api/v1/users/login', limiter);
 
 app.use('/api/v1/categories', categoryRoute);
 app.use('/api/v1/products', productRoute);
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Application shutting down..🔥');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Application shutting down..🔥');
+  server.close(() => {
+    process.exit(1);
+  });
 });
